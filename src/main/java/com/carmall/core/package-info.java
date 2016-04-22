79437bbba3352f99e4a1102e38d60004154f2036
gitmrzhang <402140296@qq.com>
2016-04-22 15:08:59 +0800

    基础的项目结构

---
+++ src/main/java/com/carmall/core/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * 文件名：com.carmall.core.package-info.java<br/>
+ * 创建时间：2016年4月22日 下午3:04:38<br/>
+ * 创建者：Administrator<br/>
+ * 修改者：暂无<br/>
+ * 修改简述：暂无<br/>
+ * 修改详述：
+ * <p>
+ * 暂无<br/>
+ * </p>
+ * 修改时间：暂无<br/>
+ */
+/**
+ * TODO 核心包<br/>
+ * <p>
+ * TODO 该类的详细描述<br/>
+ * <p>
+ * @author Administrator
+ * @date 2016年4月22日 下午3:04:38
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+package com.carmall.core;
--- src/main/resources/readme.txt
+++ src/main/resources/readme.txt
@@ -2,3 +2,6 @@ generatorConfig.xml中配置更改
 javaClientGenerator ： type = "ANNOTATEDMAPPER" 
 生成的对象是 MyBatis 3.x 映射器基础结构的 Java接口。 这些接口将会基于注解和 MyBatis 3.x SqlProviders。不会有XML文件生成。（注：就是纯接口使用注解的形式，不会有XML文件）
 ANNOTATEDMAPPER 依赖 MyBatis 3.0.4 或更高版本。targetRuntime=MyBatis3
+
+1.1新增
+基本的Service dao
